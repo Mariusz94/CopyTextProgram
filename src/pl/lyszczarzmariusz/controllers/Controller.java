962287b0e5366f9116f6dd204b6842d6b5fedf86
Mariusz Lyszczarz <lyszczarz.mariusz@gmail.com>
2019-10-31 14:23:33 +0100

    Initial commit

---
+++ src/pl/lyszczarzmariusz/controllers/Controller.java
@@ -0,0 +1,85 @@
+package pl.lyszczarzmariusz.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+
+public class Controller {
+    Clipboard clipboard;
+    StringSelection selection;
+
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+
+    @FXML
+    private TextField in1;
+    @FXML
+    private TextField in2;
+    @FXML
+    private TextField in3;
+    @FXML
+    private TextField in4;
+    @FXML
+    private TextField in5;
+
+    public Controller() throws AWTException {
+        clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        System.out.println("Zainicjalizowany Controller");
+    }
+
+    @FXML
+    void initialize(){
+        button1.setText("Copy");
+        button2.setText("Copy");
+        button3.setText("Copy");
+        button4.setText("Copy");
+        button5.setText("Copy");
+    }
+
+    @FXML
+    public void onActionButton1(){
+        copyText(in1);
+    }
+
+    @FXML
+    public void onActionButton2(){
+        copyText(in2);
+    }
+
+    @FXML
+    public void onActionButton3(){
+        copyText(in3);
+    }
+
+    @FXML
+    public void onActionButton4(){
+        copyText(in4);
+    }
+
+    @FXML
+    public void onActionButton5(){
+        copyText(in5);
+    }
+
+    private void copyText(TextField in) {
+        String text = in.getText();
+        if(text == null){
+            text = "null";
+        }
+        StringSelection selection = new StringSelection(text);
+        clipboard.setContents(selection, null);
+    }
+}
---
+++ src/pl/lyszczarzmariusz/services/Main.java
@@ -0,0 +1,33 @@
+package pl.lyszczarzmariusz.services;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import pl.lyszczarzmariusz.controllers.Controller;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("../view/sample.fxml"));
+        StackPane stackPane = loader.load();
+
+        Controller controller = loader.getController();
+
+        Scene scene = new Scene(stackPane, 243, 168);
+
+        primaryStage.setScene(scene);
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.setTitle("Copy Text");
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
